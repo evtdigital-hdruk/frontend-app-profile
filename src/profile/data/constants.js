2bc959866f8031e367c1df6648b7917128d25073
Will <will@evt.digital>
2024-08-29 17:01:31 +0100

    Adding profession categories and job titles to constants

--- src/profile/data/constants.js
+++ src/profile/data/constants.js
@@ -22,7 +22,50 @@ const SOCIAL = {
   },
 };
 
+const PROFESSION_CATEGORIES = [
+  'academia',
+  'funder',
+  'government_public_sector',
+  'healthcare_nhs',
+  'healthcare_private',
+  'industry',
+  'media_communication',
+  'ngo',
+  'public',
+  'research_institute',
+  'researcher',
+  'student',
+  'other',
+];
+
+const JOB_TITLES = [
+  'clinician',
+  'data_governance_and_privacy_professional',
+  'early_career_researcher',
+  'fellow',
+  'health_data_analyst',
+  'health_data_informatician',
+  'health_data_scientist',
+  'manager_administrator',
+  'media_communications_professional',
+  'msc_student',
+  'phd_student',
+  'policy',
+  'ppie_professional',
+  'professor_lecturer',
+  'project_programme_manager',
+  'research_coordinator',
+  'research_technical_professional',
+  'researcher',
+  'statistician',
+  'undergraduate_student',
+  'not_employed',
+  'other',
+];
+
 export {
   EDUCATION_LEVELS,
   SOCIAL,
+  PROFESSION_CATEGORIES,
+  JOB_TITLES,
 };
